6cea5fb043f6727efcc001dd91ed036a605e64cb
Lickton <lilihuanmao@gmail.com>
2024-01-30 21:36:27 +0800

    sim IR in RTL

--- src/main/resources/ifetch.v
+++ src/main/resources/ifetch.v
@@ -1,14 +1,14 @@
 import "DPI-C" function void pmem_read(input int paddr, output int rword);
 
 module ifetch (
-    // input           clk,
-    // input           rst,
+    input           clock,
+    input           reset,
     input  [31:0]   addr,
 
     output [31:0]   inst
 );
 
-always@(posedge clk) begin
+always@(posedge clock) begin
     pmem_read(addr, inst);
 end
     
